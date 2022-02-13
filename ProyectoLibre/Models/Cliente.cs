a260a733c99f37c2e65c35c4912bd05a6ac15aa0
Eder Savillon <edersavillon1998@gmail.com>
2022-02-13 02:05:07 -0600

    Add project files.

---
+++ ProyectoLibre/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ProyectoLibre.Models
+{
+    public partial class Cliente
+    {
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Telefono { get; set; }
+        public string Direccion { get; set; }
+    }
+}
